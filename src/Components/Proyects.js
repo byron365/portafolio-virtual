c4836191c20537eefa01194c48e61e69b123e8ad
byron365 <byronalas365@gmail.com>
2023-02-21 09:09:31 -0600

    Quitamos por el momento el segundo proyecto a causa de inconvenientes con heroku

--- src/Components/Proyects.js
+++ src/Components/Proyects.js
@@ -12,7 +12,7 @@ export const Proyects = () => {
         <div className="card card-body">
           <h4>Descripción:</h4><br/>
           <p>
-            <a href='https://dlmp3-proyect.herokuapp.com/' target='_blank'><strong>DLMP3</strong></a> es una página web con la que puedes descargar musica y videos desde YouTube, 
+            <a href='https://dlmp3online.com/home' target='_blank'><strong>DLMP3</strong></a> es una página web con la que puedes descargar musica y videos desde YouTube, 
             únicamente ingresando la url del video y seleccionando el formato deseado, 
             entre los formatos de audio que se pueden descargar estan: mp3, wma, aac, ogg, 
             flac, para los formatos de video: mp4, avi wmv, mov, mkv, ademas se puede elegir 
@@ -51,16 +51,16 @@ export const Proyects = () => {
               <li>Heroku: Plataforma en la cual se desplegó el contenedor con la aplicación. </li>
           </ul>
           <img src='https://res.cloudinary.com/dfxizywkv/image/upload/v1653323275/img/futuro_nizfhm.png'/>
-          <p style={{textAlign: 'center'}}><cite>(Resultado futuro), <a href='https://dlmp3-proyect.herokuapp.com/' target='_blank'><strong>Resultado actual</strong></a> (puede demorar unos segundos al inicio).</cite></p>
+          <p style={{textAlign: 'center'}}><cite>(Resultado futuro), <a href='https://dlmp3online.com/home' target='_blank'><strong>Resultado actual</strong></a> (puede demorar unos segundos al inicio).</cite></p>
         </div>
       </div>
 
-      <button className="category-item transition item-proyect" type="button" data-bs-toggle="collapse" data-bs-target="#collapseProyect2" aria-expanded="false" aria-controls="collapseProyect">
+      {/* <button className="category-item transition item-proyect" type="button" data-bs-toggle="collapse" data-bs-target="#collapseProyect2" aria-expanded="false" aria-controls="collapseProyect">
         MyNotes
       </button>
-      <hr/>
+      <hr/> */}
       
-      <div className="collapse" id="collapseProyect2">
+      {/* <div className="collapse" id="collapseProyect2">
         <div className="card card-body">
           <h4>Descripción:</h4><br/>
           <p>
@@ -92,7 +92,7 @@ export const Proyects = () => {
               <li>Heroku: Plataforma en la cual se desplegó la aplicación y la base de datos. </li>
           </ul>
         </div>
-      </div>
+      </div> */}
 
     </div>
   )
